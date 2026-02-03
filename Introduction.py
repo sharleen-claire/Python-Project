dc8bac2a3438cd9315161c5d2c4d6bebd414f1d4
sharleen-claire <clairesharleen17@gmail.com>
2026-02-03 13:20:16 +0300

    initial commit

---
+++ Introduction.py
@@ -0,0 +1,6 @@
+#Print is a method used to display an output
+
+print("Welcome to Python Programming")
+print("Sharleen")
+print(200)
+print(40*3)
---
+++ Variables.py
@@ -0,0 +1,20 @@
+#Variable is a container used to store a value
+
+school = "eMobilis"
+firstname ="john"
+lastname ="Smith"
+num1 =45
+num2 =100
+language ="Python"
+
+print(num1 + num2)
+print(school)
+print(firstname)
+print(lastname)
+print(firstname,lastname)
+print(school,"is a coding school")
+print(language,"python is a web programming language")
+
+print(num1 +num2)
+print("The sum of" ,num1, "and", num2, "is",num1+num2)
+
