f4c08066a032077a89b261245477949b702ca93f
sharleen-claire <clairesharleen17@gmail>
2026-02-05 11:01:55 +0300

    initial commit

---
+++ datatypes.py
@@ -0,0 +1,33 @@
+age= 20 #Interger
+length = 45.6 #Float
+greeting ="Hello there" #String
+hasfeathers = True #Boolean
+
+#Data Structures- Multiple values stored in
+#one variable.
+fruits = ["Banana","Mango","Pineapple"] #List - Ordered and changeable
+courses =["MIT","DataScience", "Cybersecurity"] #Array -similar datatypes
+cars =  ("Mercedes" ,"Ford","Nissan", "Bughati") #Tuple - Ordered and unchangeable
+countries ={"Zambia","Canada","India"} #set - unordered and unchangeable
+student ={
+    "firstname": "Esther",
+    "course": "MIT",
+    "age" : 19,
+    "nationality" :"Kenya",
+    "gender" : "Female"
+
+} #Dictionary -Key Value Pair
+
+
+print(age)
+print("The length is ",length)
+print(fruits)
+print(countries)
+print(student["nationality"])
+
+#Typecasting - converting one data type to another
+print(float(age))
+print(int(length))
+
+
+
---
+++ strings.py
@@ -0,0 +1,13 @@
+greeting ="Hello"
+course ="SOFTWARE ENGINEERING"
+
+print(greeting[0])
+
+print(len(greeting))
+
+print(course.lower())
+
+print(greeting.upper())
+
+#string concatenation-joinig strings
+print(greeting +course)
