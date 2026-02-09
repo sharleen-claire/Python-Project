f06b2d91e4de6cb9eab0750f054172558debf771
sharleen-claire <clairesharleen17@gmail>
2026-02-09 13:44:51 +0300

    initial commit

--- calculator.py
+++ calculator.py
@@ -1,2 +1,16 @@
-num1 = int(input("Enter a number: "))
+num1 = float(input("Enter a number: "))
+operators =input("Enter operators(+, -,*,/): ")
+num2 = float(input("Enter second number: "))
+
+if operators == "+":
+    print(num1 + num2)
+elif operators == "-":
+    print(num1 - num2)
+elif operators == "*":
+    print(num1 * num2)
+elif operators == "/":
+    print(num1 / num2)
+else:
+    print("Invalid operator")
+
 
